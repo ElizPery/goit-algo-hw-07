544b89bf9ac9013dae9feb623216284253c74412
Elizabeth Pereriadkina <el.pereryadkina@gmail.com>
2025-01-27 18:07:32 +0000

    add realization of binary tree

---
+++ binary_tree.py
@@ -0,0 +1,83 @@
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+
+def insert(root: Node, key: int):
+    if root is None:
+        return Node(key)
+    else:
+        if key < root.val:
+            root.left = insert(root.left, key)
+        else:
+            root.right = insert(root.right, key)
+    return root
+
+
+def search(root: Node, key: int):
+    if root is None or root.val == key:
+        return root
+    if key < root.val:
+        return search(root.left, key)
+    return search(root.right, key)
+
+
+def min_value_node(node: Node):
+    current = node
+    while current.left:
+        current = current.left
+    return current
+
+
+def max_value_node(node: Node):
+    current = node
+    while current.right:
+        current = current.right
+    return current.val
+
+
+def preorder_traversal(root: Node, path: list = []):
+    if root:
+        path.append(root.val)
+        preorder_traversal(root.left, path)
+        preorder_traversal(root.right, path)
+    return sum(path)
+
+
+def delete(root: Node, key: int):
+    if not root:
+        return root
+
+    if key < root.val:
+        root.left = delete(root.left, key)
+    elif key > root.val:
+        root.right = delete(root.right, key)
+    else:
+        if not root.left:
+            temp = root.right
+            root = None
+            return temp
+        elif not root.right:
+            temp = root.left
+            root = None
+            return temp
+        root.val = min_value_node(root.right).val
+        root.right = delete(root.right, root.val)
+    return root
+
+
+# Test
+root = Node(5)
+root = insert(root, 3)
+root = insert(root, 2)
+root = insert(root, 4)
+root = insert(root, 7)
+root = insert(root, 6)
+root = insert(root, 8)
+root = delete(root, 7)
+
+print(min_value_node(root).val)
+print(max_value_node(root))
+print(preorder_traversal(root))
